b98390778d6593e94ed00c7e04dbe2e635b28c61
Miriam.Chiarolanza <miriam.chiarolanza@alten.it>
2023-01-19 17:07:58 +0100

    refactor: divide runGame() in multiple methods in GameController.

--- zoogame/src/game/GameController.java
+++ zoogame/src/game/GameController.java
@@ -5,8 +5,6 @@ import command.CommandFactory;
 import game.console.InputController;
 import model.game.Bag;
 import model.game.Player;
-import model.labyrinth.Direction;
-import model.labyrinth.Door;
 import model.labyrinth.Labyrinth;
 import model.labyrinth.Room;
 
@@ -33,10 +31,6 @@ public class GameController {
 		return player;
 	}
 
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
-
 	public Room getCurrentRoom() {
 		return currentRoom;
 	}
@@ -50,65 +44,61 @@ public class GameController {
 	}
 
 	public void runGame() {
-		try {
-				String input;
-				int labyrinthDimension = 0;
-				Labyrinth labyrinth;
+		String input;
+		registerPlayer();
+		System.out.println("Welcome to Afantasia, our fantastic world without fantasy!\n" +
+				"In this game, you're lost in a labyrinth and you have to find the exit.\n" +
+				"In your path you will find animals and objects to collect.\n");
+		Labyrinth labyrinth = addLabyrinthToGame(chooseLabirinthDimension());
+		System.out.println("You are now in the entry room. You can type your commands using the keyboard: write help for a list of possible commands.\n\n" +
+				currentRoom.description() + "\n");
 
-				System.out.println("Please enter your name");
-				System.out.print(">");
-				input = InputController.readString();
-				player = new Player(input,100, new Bag());
-				System.out.println("Hello " + player.getName() + "! Welcome to Afantasia, our fantastic world without fantasy!\n" +
-						"In this game, you're lost in a labyrinth and you have to find the exit.\n" +
-						"In your path you will find animals and objects to collect.\n");
-
-				while (labyrinthDimension > 100 || labyrinthDimension < 5) {
-					System.out.println("Please insert a number between 5 and 100 to define the dimension of the labyrinth: ");
-					System.out.print(">");
-					input = InputController.readString();
-					try {
-						labyrinthDimension = Integer.parseInt(input);
-					} catch (NumberFormatException e) {
-						System.err.println("You must type a number!!!!!");
-					}
-				}
-
-			labyrinth = new Labyrinth(14); //nRooms is constant because we don't have enough animals and items
-			labyrinth.addAnimalsAndItemsToLabyrinth();
-			currentRoom = labyrinth.getRooms().get(0);
-			System.out.println("Congratulations, a labyrinth with " + labyrinth.getRooms().size() + " rooms has been created ;-) . Who do you think you are to take such a decision!?\n" +
-					"You are now in the entry room. You can type your commands using the keyboard: write help for a list of possible commands.\n\n" +
-					currentRoom.description() + "\n");
-
-			while (!gameEnded) {
-				String output = "";
-				System.out.print(">");
-				input = InputController.readString();
-				String[] splitInput = input.toLowerCase().split(" ", 2);
-				try {
-					Command command = CommandFactory.make(splitInput, instance);
-					output = command.execute();
-				} catch (Exception e) {
-					output = e.getMessage();
-				}
-
-				System.out.println(output + "\n");
+		while (!gameEnded) {
+			String output = "";
+			System.out.print(">");
+			input = InputController.readString();
+			String[] splitInput = input.toLowerCase().split(" ", 2);
+			try {
+				Command command = CommandFactory.make(splitInput, instance);
+				output = command.execute();
+			} catch (Exception e) {
+				output = e.getMessage();
 			}
-			}catch(Exception e){
-				e.getMessage();
+
+			System.out.println(output + "\n");
+		}
+	}
+
+	private void registerPlayer(){
+		String input;
+		System.out.println("Please enter your name");
+		System.out.print(">");
+		input = InputController.readString();
+		player = new Player(input,100, new Bag());
+		System.out.println("Hello " + player.getName() + "!");
+	}
+
+	private int chooseLabirinthDimension(){
+		String input;
+		int labyrinthDimension = 0;
+		while (labyrinthDimension > 14 || labyrinthDimension < 5) { //max labyrinthDimension is 14 because we don't have enough animals and items for bigger dimensions
+			System.out.println("Please insert a number between 5 and 100 to define the dimension of the labyrinth: ");
+			System.out.print(">");
+			input = InputController.readString();
+			try {
+				labyrinthDimension = Integer.parseInt(input);
+			} catch (NumberFormatException e) {
+				System.err.println("You must type a number!!!!!");
 			}
 		}
+		return labyrinthDimension;
+	}
 
-	public Room goToNextRoom(Room currentRoom, String direction){
-		try{
-			Door requestedDoor = currentRoom.getDoors().get(Direction.valueOf(direction.toUpperCase()));
-			if(requestedDoor == null){
-				return currentRoom;
-			}
-			return requestedDoor.nextRoom(currentRoom);
-		}catch(IllegalArgumentException e){
-			return currentRoom;
-		}
+	private Labyrinth addLabyrinthToGame(int labyrinthDimesion){
+		Labyrinth labyrinth = new Labyrinth(labyrinthDimesion);
+		labyrinth.addAnimalsAndItemsToLabyrinth();
+		currentRoom = labyrinth.getRooms().get(0);
+		System.out.println("This labyrinth has " + labyrinth.getRooms().size() + " rooms.\n");
+		return labyrinth;
 	}
 }
