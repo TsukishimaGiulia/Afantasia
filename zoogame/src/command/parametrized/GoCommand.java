c5f93296a5bacd64d5bf20072c77c34a4c88878e
Miriam.Chiarolanza <miriam.chiarolanza@alten.it>
2023-01-19 15:04:06 +0100

    refactor: optimise execute() in GoCommand.

--- zoogame/src/command/parametrized/GoCommand.java
+++ zoogame/src/command/parametrized/GoCommand.java
@@ -6,24 +6,21 @@ import model.labyrinth.Room;
 
 public class GoCommand extends ParametrizedCommand {
 
-
     public GoCommand(GameController gc, String[] command) throws MissingParameterException {
         super(gc, command);
     }
     @Override
     public String execute(){
-        String output = "";
         Room room = gc.goToNextRoom(gc.getCurrentRoom(), command[1]);
-        if (room.equals(gc.getCurrentRoom()) || room == null) {
-            output = "The selected room does not exist. You still are in " + gc.getCurrentRoom().getName() + ".";
+        if (room.equals(gc.getCurrentRoom())) {
+            return "The selected room does not exist. You still are in " + gc.getCurrentRoom().getName() + ".";
         } else {
             gc.setCurrentRoom(room);
-            output = "You have just stepped into " + gc.getCurrentRoom().description();
             if (gc.getCurrentRoom().isExit()) {
-                output = "Congratulations, " + gc.getPlayer().getName() + ", you've found the exit!";
                 gc.setGameEnded(true);
+                return "Congratulations, " + gc.getPlayer().getName() + ", you've found the exit!";
             }
+            return "You have just stepped into " + gc.getCurrentRoom().description();
         }
-        return output;
     }
 }
